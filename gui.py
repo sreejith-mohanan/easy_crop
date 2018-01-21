3f5ae500c65f7838c7d5f626c13bd7ab36324dff
Sreejith Mohanan <sree.rfx@gmail.com>
2018-01-21 13:13:47 +0530

    fixed windows issue, added 8x8

--- gui.py
+++ gui.py
@@ -23,7 +23,9 @@ class AutoScrollbar(Scrollbar):
 
 
 class Application():
-    def __init__(self, main):
+    def __init__(self, main, win_width=900, win_height=540):
+
+        self.os = os.name
 
         # list of image paths
         self.image_list = []
@@ -53,14 +55,14 @@ class Application():
         self.image_height = 0
 
         self.frame = Frame(main)
-        self.frame.grid(row=0, column=0, columnspan=6)
+        self.frame.grid(row=0, column=0, columnspan=8, sticky=N+W+S+E)
         self.frame.grid_rowconfigure(0, weight=1)
         self.frame.grid_columnconfigure(0, weight=1)
 
         self.scrollbar_x = AutoScrollbar(self.frame, orient=HORIZONTAL)
         self.scrollbar_y = AutoScrollbar(self.frame)
 
-        self.canvas = Canvas(self.frame, xscrollcommand=self.scrollbar_x.set, yscrollcommand=self.scrollbar_y.set, width=900, height=540, relief=FLAT, bg='white')
+        self.canvas = Canvas(self.frame, xscrollcommand=self.scrollbar_x.set, yscrollcommand=self.scrollbar_y.set, width=win_width, height=win_height, relief=FLAT, bg='white')
         self.canvas.grid(row=0, column=0)
 
         self.scrollbar_x.config(command= self.canvas.xview)
@@ -80,6 +82,8 @@ class Application():
         main.bind('<Down>', self.on_down_arrow)
         main.bind('<Return>', self.on_enter_key)
         main.bind('<space>', self.on_space_key)
+
+        # main.bind('<Configure>', self.on_resize_window)
 
         # binding mouse event to drag the crop window
         self.canvas.bind('<Motion>', self.on_mouse_move)
@@ -102,39 +106,43 @@ class Application():
 
         #---->
 
-        self.image_on_canvas = self.canvas.create_image(0, 0, anchor=NW, image=self.photo_img)
+        self.image_on_canvas = self.canvas.create_image(win_width/2, win_height/2, anchor=CENTER, image=self.photo_img)
 
         self.rectangle = self.canvas.create_rectangle(self.rect_x, self.rect_y, 32, 32, outline='white')
 
         # first row
         self.directory_label = Label(main, textvariable= self.data_path, relief=FLAT)
-        self.directory_label.grid(row=1, column=0, columnspan=6)
+        self.directory_label.grid(row=1, column=0, columnspan=8)
         self.button_load_dir = Button(main, text='Folder Load', command=self.load_data_dir)
-        self.button_load_dir.grid(row=2, column=0, columnspan=6)
+        self.button_load_dir.grid(row=2, column=0, columnspan=8)
 
         # second row
-        self.size_32_radio = Radiobutton(main, text="32x32", variable=self.size_value, value=32, command=self.size_change)
-        self.size_32_radio.grid(row=3, column=0, sticky=E+W)
-        self.size_64_radio = Radiobutton(main, text="64x64", variable=self.size_value, value=64, command=self.size_change)
-        self.size_64_radio.grid(row=3, column=1, sticky=E+W)
+        self.size_8_radio = Radiobutton(main, text="8x8      ", variable=self.size_value, value=8, command=self.size_change)
+        self.size_8_radio.grid(row=3, column=0, sticky=E+W)
+        self.size_16_radio = Radiobutton(main, text="16x16   ", variable=self.size_value, value=16, command=self.size_change)
+        self.size_16_radio.grid(row=3, column=1, sticky=E+W)
+        self.size_32_radio = Radiobutton(main, text="32x32   ", variable=self.size_value, value=32, command=self.size_change)
+        self.size_32_radio.grid(row=3, column=2, sticky=E+W)
+        self.size_64_radio = Radiobutton(main, text="64x64   ", variable=self.size_value, value=64, command=self.size_change)
+        self.size_64_radio.grid(row=3, column=3, sticky=E+W)
         self.size_128_radio = Radiobutton(main, text="128x128", variable=self.size_value, value=128, command=self.size_change)
-        self.size_128_radio.grid(row=3, column=2, sticky=E+W)
+        self.size_128_radio.grid(row=3, column=4, sticky=E+W)
         self.size_256_radio = Radiobutton(main, text="256x256", variable=self.size_value, value=256, command=self.size_change)
-        self.size_256_radio.grid(row=3, column=3, sticky=E+W)
+        self.size_256_radio.grid(row=3, column=5, sticky=E+W)
         self.size_512_radio = Radiobutton(main, text="512x512", variable=self.size_value, value=512, command=self.size_change)
-        self.size_512_radio.grid(row=3, column=4, sticky=E+W)
+        self.size_512_radio.grid(row=3, column=6, sticky=E+W)
         self.button_save = Button(main, text='Save', command=self.save_croped_image, fg='red')
-        self.button_save.grid(row=3, column=5, sticky=E+W)
+        self.button_save.grid(row=3, column=7, sticky=E+W)
 
         # third row
         self.button_previous = Button(main, text='Previous', command=self.show_previous)
-        self.button_previous.grid(row=4, column=0, columnspan=3, sticky=E+W)
+        self.button_previous.grid(row=4, column=0, columnspan=4, sticky=E+W)
         self.button_next = Button(main, text='Next', command=self.show_next)
-        self.button_next.grid(row=4, column=3, columnspan=3, sticky=E+W)
+        self.button_next.grid(row=4, column=4, columnspan=4, sticky=E+W)
 
         #fourth row
         self.button_load_img = Button(main, text='File Open', command=self.load_img_random)
-        self.button_load_img.grid(row=5, columnspan=6, sticky=E+W)
+        self.button_load_img.grid(row=5, columnspan=8, sticky=E+W)
 
     def load_img(self, filename):
         """
@@ -146,7 +154,8 @@ class Application():
         self.image_width = self.img.width
         self.image_height = self.img.height
         self.photo_img = ImageTk.PhotoImage(self.img)
-        self.canvas.itemconfig(self.image_on_canvas, image=self.photo_img)
+        self.canvas.itemconfig(self.image_on_canvas, anchor=NW, image=self.photo_img)
+        self.canvas.coords(self.image_on_canvas, 0,0)
 
         self.canvas.config(scrollregion=(0, 0, self.image_width, self.image_height))
         print(self.current_image_index, '|', filename)
@@ -179,7 +188,10 @@ class Application():
         """
         root_dir = str(self.data_path.get())
         x1, y1, x2, y2 = self.canvas.coords(self.rectangle)
-        filename = (self.image_list[self.current_image_index].split('/')[-1]).split('.')[0]
+        if self.os == 'nt':
+            filename = (self.image_list[self.current_image_index].split('\\')[-1]).split('.')[0]
+        else:
+            filename = (self.image_list[self.current_image_index].split('/')[-1]).split('.')[0]
 
         try:
             img2 = self.img.crop((x1, y1, x2, y2))
@@ -202,6 +214,12 @@ class Application():
         if window_size == 32:
             img2.save(root_dir + "/32x32/" + filename + '_' + str(self.crop_count) + ".png")
             print('saving to: ', root_dir + "/32x32/" + filename + '_' + str(self.crop_count) + ".png")
+        if window_size == 16:
+            img2.save(root_dir + "/16x16/" + filename + '_' + str(self.crop_count) + ".png")
+            print('saving to: ', root_dir + "/16x16/" + filename + '_' + str(self.crop_count) + ".png")
+        if window_size == 8:
+            img2.save(root_dir + "/8x8/" + filename + '_' + str(self.crop_count) + ".png")
+            print('saving to: ', root_dir + "/8x8/" + filename + '_' + str(self.crop_count) + ".png")
 
         self.crop_count += 1
 
@@ -234,7 +252,12 @@ class Application():
         """
         root_dir = str(self.data_path.get())
         # create directories if not exist
-        print('creating directories /32x32/ /64x64/ /128x128/ /256x256/')
+        print('creating directories /8x8/ /16x16/ /32x32/ /64x64/ /128x128/ /256x256/')
+
+        if not os.path.exists(root_dir + '/8x8/'):
+            os.makedirs(root_dir + '/8x8/')
+        if not os.path.exists(root_dir + '/16x16/'):
+            os.makedirs(root_dir + '/16x16/')
         if not os.path.exists(root_dir + '/32x32/'):
             os.makedirs(root_dir + '/32x32/')
         if not os.path.exists(root_dir + '/64x64/'):
@@ -313,6 +336,10 @@ class Application():
         """
         print('increasing')
         window_size = int(self.size_value.get())
+        if window_size == 8:
+            self.size_value.set(16)
+        if window_size == 16:
+            self.size_value.set(32)
         if window_size == 32:
             self.size_value.set(64)
         if window_size == 64:
@@ -339,9 +366,14 @@ class Application():
             self.size_value.set(64)
         if window_size == 64:
             self.size_value.set(32)
+        if window_size == 32:
+            self.size_value.set(16)
+        if window_size == 16:
+            self.size_value.set(8)
         self.size_change()
 
 
 master = Tk()
-Application(master)
+# master.resizable(width=False, height=False)
+Application(master, 1000, 540)
 mainloop()
